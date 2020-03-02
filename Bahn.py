147db2c50e129e1e35f0518eaa1b0a4514d61be8
Henry440 <440henry@gmail.com>
2020-03-02 21:32:39 +0100

    Search Function

--- Bahn.py
+++ Bahn.py
@@ -1,17 +1,24 @@
 import json
 import requests
 
-search = input("Suche : ")
+def station_search():
+    search = input("Suche : ")
+    url = "https://marudor.de/api/hafas/v1/station/" + search
+    param = dict()
+    resp = requests.get(url=url, params=param)
+    data = resp.json()
 
-url = "https://marudor.de/api/hafas/v1/station/" + search
-param = dict()
+    stations = [""]
+    evaID = [""]
 
-resp = requests.get(url=url, params=param)
-data = resp.json()
+    count = 0
+    for x in data:
+        count = count + 1
+    for x in range(count):
+        print(data[x]["id"] + " - " + data[x]["title"])
+        stations.append(data[x]["title"])
+        evaID.append(data[x]["id"])
+    return (stations, evaID)
 
-count = 0
-for x in data:
-    count = count + 1
-
-for x in range(count):
-    print(data[x]["id"] + " - " + data[x]["title"])
+station = station_search()
+print(station[0][0])
