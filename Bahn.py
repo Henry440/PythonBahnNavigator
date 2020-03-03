a918c902df10cf7b7eb98d6aec52ec529c2ef127
Oliver Audehm <440henry@gmail.com>
2020-03-03 16:17:05 +0100

    Neue Funktionen
    Add new Functions.

--- Bahn.py
+++ Bahn.py
@@ -2,12 +2,14 @@ import json
 import requests
 
 def station_search():
-    search = input("Suche : ")
-    url = "https://marudor.de/api/hafas/v1/station/" + search
-    param = dict()
-    resp = requests.get(url=url, params=param)
-    data = resp.json()
+    search = raw_input("Suche : ")
+    search = str(search)
+    #url = "https://marudor.de/api/hafas/v1/station/" + search
+    url = "https://marudor.de/api/station/v1/search/" + search
 
+    param = dict()
+    resp = requests.get(url = url, params=param)
+    data = resp.json()
     stations = [""]
     evaID = [""]
 
@@ -15,10 +17,39 @@ def station_search():
     for x in data:
         count = count + 1
     for x in range(count):
-        print(data[x]["id"] + " - " + data[x]["title"])
         stations.append(data[x]["title"])
         evaID.append(data[x]["id"])
+    stations.remove("")
+    evaID.remove("")
     return (stations, evaID)
 
-station = station_search()
-print(station[0][0])
+def selectStation(data):
+    stations = data[0]
+    evaID = data[1]
+    i = 1
+    for st in stations:
+        print(str(i) + ") " + st)
+        i = i + 1
+
+    auswahl = int(raw_input("Station Waehlen : "))
+    auswahl = auswahl - 1
+    print(stations[auswahl])
+    return auswahl
+
+def abfahrten(eva):
+    url = "https://marudor.de/api/iris/v1/abfahrten/" + eva
+    print(eva)
+    param = dict()
+    resp = requests.get(url = url, params=param)
+    data = resp.json()
+    fahrten = data["departures"]
+    for x in fahrten:
+        try:
+            print(x["arrival"])
+        except Exception:
+            print(x)
+
+data = station_search()
+auswahl = selectStation(data)
+auswahl = int(auswahl)
+abfahrten(data[1][auswahl])
