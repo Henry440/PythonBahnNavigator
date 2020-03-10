88c36a26daff792a7d1d850fbff7d9277b8b6950
henry440 <440henry@gmail.com>
2020-03-10 13:47:34 +0100

    Add Monitoring Function

--- Bahn.py
+++ Bahn.py
@@ -2,6 +2,7 @@ import json
 import requests
 import sys
 from datetime import datetime
+from time import sleep
 
 import ZugDaten as train
 
@@ -45,6 +46,56 @@ def selectStation(data):
         auswahl = int(input("Station Waehlen : "))
     auswahl = auswahl - 1
     return auswahl
+
+def monitoring():
+    stationen = station_search()
+    auswahl = int(selectStation(stationen))
+    delay = 0
+    zyklen = 0
+    try:
+        delay = raw_input("Monitoring Warten [Minuten] : ")
+        zyklen = raw_input("Monitoring Zyklen : ")
+    except Exception:
+        delay = input("Monitoring Warten [Minuten] : ")
+        zyklen = raw_input("Monitoring Zyklen : ")
+
+    for i in range(1, int(zyklen)):
+        daten = bahnhofsdaten(stationen[1][auswahl])
+        current = daten["departures"]
+        for line in current:
+
+            zug = line["train"]["name"]
+            von = line["route"][0]["name"]
+            nach = line["destination"]
+            try:
+                an = line["arrival"]["time"]
+            except Exception as e:
+                an = "N.A."
+            ab = line["departure"]["time"]
+            try:
+                delayAn = line["arrival"]["delay"]
+            except Exception as e:
+                delayAn = "N.A."
+            try:
+                delayAb = line["departure"]["delay"]
+            except Exception as e:
+                delayAb = "N.A."
+            gleisPlan = line["arrival"]["scheduledPlatform"]
+            gleis = line["arrival"]["platform"]
+            try:
+                ausfallAn = line["arrival"]["cancelled"]
+            except Exception as e:
+                ausfallAn = "N.A."
+            try:
+                ausfallAb = line["departure"]["cancelled"]
+            except Exception as e:
+                ausfallAb = "N.A."
+
+            print(zug + " | " + von + " --> " + nach + " | An : " + str(timestamp_to_time(an)) + " Ab : " + str(timestamp_to_time(ab)) + " | Gleis : " + gleis + " | Delay : " + str(delayAn))
+
+        print("---Log : " + str(i) + "-------------------------------------------------")
+        sleep(int(delay) * 60)
+
 
     #departure
 def printAbfahrt(datas):
@@ -102,6 +153,7 @@ def timestamp_to_time(timestamp):
 while True:
     print("Auswahl : ")
     print("Ankuenfte [1] | Abfahrten [2] | Verspaetung [3]")
+    print("Monitoring [4]")
     print("Beenden [99]")
 
     choice = 1
@@ -125,6 +177,8 @@ while True:
         else:
             print("Fehler ungueltige Aktion")
             sys.exit()
+    elif(choice == 4):
+        monitoring();
     elif(choice == 99):
         print("Beende")
         sys.exit()
